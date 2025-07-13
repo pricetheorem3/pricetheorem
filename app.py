722f07d3f92d82067ff897941cdac22bfb23d277
pricetheorem3 <pricetheorem@gmail.com>
2025-07-13 20:12:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-
-from flask import Flask, render_template, jsonify
-import os
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    api_key = os.environ.get("KITE_API_KEY", None)
-    if not api_key:
-        return "⚠️ Kite API Key not set in environment."
-    return render_template("index.html", kite_api_key=api_key)
+    return render_template('index.html')
