d355bf58071b198098a60b5a3aa196a2603afcbc
pricetheorem3 <pricetheorem@gmail.com>
2025-07-13 20:20:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask, render_template, current_app
+import os
+
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
